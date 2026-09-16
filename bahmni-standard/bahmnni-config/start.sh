4c77399d59b12398a02bda587bbc2dfab42e69f8
Ifeoluwa Andrew Omole <iomole3@gmail.com>
2026-09-16 16:18:13 +0100

    Use local built images for proxy and config for easier custom configuration

---
+++ bahmni-standard/bahmnni-config/start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Commented out by andy-ife because these lines were causing all changes made to the config to be lost
+
+# set -e
+# echo "Rewriting Bahmni Config at /usr/local/bahmni_config"
+# cp -r /etc/bahmni_config /usr/local/
+
+echo "Sleeping infinitely....."
+sleep infinity
